f671e5ef3b628931a62271bfbc78972c8dcbb790
wangwen118 <yp_wangwen@163.com>
2019-05-12 22:00:18 +0800

    100.py

--- 100.py
+++ 100.py
@@ -68,7 +68,7 @@
 # if __name__ == '__main__':
 #     import time
 #     import random
-    
+	
 #     play_it = input('do you want to play it.(\'y\' or \'n\')')
 #     while play_it == 'y':
 #         c = input('input a character:\n')
@@ -274,3 +274,28 @@
 '''
 	有n个人围成一圈，顺序排号。从第一个人开始报数（从1到3报数），凡报到3的人退出圈子，问最后留下的是原来第几号的那位
 '''
+
+# 68
+'''
+	有n个整数，使其前面各数顺序向后移m个位置，最后m个数变成最前面的m个数
+'''
+if __name__ == '__main__':
+	n = int(input('整数 n 为:\n'))
+	m = int(input('向后移 m 个位置为:\n'))
+ 
+	def move(array, n, m):
+		array_end = array[n - 1]
+		for i in range(n - 1,-1,- 1):
+			array[i] = array[i - 1]
+		array[0] = array_end
+		m -= 1
+		if m > 0:move(array, n, m)
+		
+	number = []
+	for i in range(n):
+		number.append(int(input('输入一个数字:\n')))
+	print('原始列表:', number)
+		
+	move(number, n, m)
+		
+	print('移动之后:', number)
